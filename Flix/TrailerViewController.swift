f169b310d681d40b83aa235a1be624344529c0fa
Katie Jiang <katiejiang@fb.com>
2017-06-22 17:56:31 -0700

    Added trailer modal without video player

---
+++ Flix/TrailerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TrailerViewController.swift
+//  Flix
+//
+//  Created by Katie Jiang on 6/22/17.
+//  Copyright © 2017 Katie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class TrailerViewController: UIViewController {
+
+    @IBAction func dismissModal(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Flix is a movies app using the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
 
-Time spent: 8 hours spent in total
+Time spent: 9 hours spent in total
 
 ## User Stories
 
@@ -16,7 +16,7 @@ The following **required** functionality is complete:
 The following **optional** features are implemented:
 
 - [x] User sees an error message when there's a networking error.
-- [ ] Movies are displayed using a CollectionView instead of a TableView.
+- [x] Movies are displayed using a CollectionView instead of a TableView.
 - [ ] User can search for a movie.
 - [ ] All images fade in as they are loading.
 - [ ] User can view the large movie poster by tapping on a cell.
